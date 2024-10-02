d8605dbf62f90cdde47dba9a539a6a8e7ec47332
anuja.shinde@cerdoit.in <anujashinde0803@gmail.com>
2024-10-02 15:26:19 +0530

    total balancebox done

--- app/app/(root)/page.tsx
+++ app/app/(root)/page.tsx
@@ -1,7 +1,29 @@
+import HeaderBox from "@/components/headerBox";
+import TotalBalnaceBox from "@/components/totalBalnaceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>home</div>;
+  const loggedIn = { firstName: " Anuja" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Greetings"}
+            subtext="Access and manage your account and transction efficiently."
+          />
+
+          <TotalBalnaceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
